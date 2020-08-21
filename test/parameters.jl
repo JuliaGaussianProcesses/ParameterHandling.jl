3eb4724b788f2dc8aa86275be0631d32a4b61390
wt <will.tebbutt@invenialabs.co.uk>
2020-08-21 17:25:51 +0100

    Fix style

--- test/parameters.jl
+++ test/parameters.jl
@@ -1,8 +1,10 @@
 @testset "parameters" begin
+
 	@testset "Postive" begin
    		test_parameter_interface(Positive(5.0))
    		test_parameter_interface(Positive(5.0))
    	end
+
    	@testset "Fixed" begin
    		test_parameter_interface(Fixed((a=5.0, b=4.0)))
    	end
