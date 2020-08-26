e1ba9bdea0ebc550e4cbc196db7c8a66e3e2cd94
wt <will.tebbutt@invenialabs.co.uk>
2020-08-26 14:39:54 +0100

    Add docstring to flatten

--- src/flatten.jl
+++ src/flatten.jl
@@ -1,3 +1,22 @@
+"""
+    flatten(x)
+
+Returns a "flattened" representation of `x` as a vector of real numbers, and a function
+`unflatten` that takes a vector of reals of the same length and returns an object of the
+same type as `x`.
+
+`unflatten` is the inverse of `flatten`, so
+```julia
+julia> x = (randn(5), 5.0, (a=5.0, b=randn(2, 3)));
+
+julia> v, unflatten = flatten(x);
+
+julia> x == unflatten(v)
+true
+```
+"""
+function flatten end
+
 function flatten(x::Real)
     v = [x]
     unflatten_to_Real(v::Vector{<:Real}) = only(v)
