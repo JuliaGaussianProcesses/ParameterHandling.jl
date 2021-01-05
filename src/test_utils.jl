fa887fbba2dcc87489271835921e2b60d24480b8
rofinn <rory.finnegan@gmail.com>
2021-01-05 17:28:39 -0600

    Simplify flatten tests.
    - atol scales with the precision being tested
    - Float64 assumptions should remain the same now
    - Don't add equality and eltypes kwargs until they're required externally
    - Using isapprox with an appropriate atol should remove the need for rounding
    - Bump patch release since the test suite isn't exported anyways.

--- src/test_utils.jl
+++ src/test_utils.jl
@@ -7,56 +7,72 @@ using Test
 using ParameterHandling: AbstractParameter, value
 
 # Handles equality of scalars, functions, tuples or arbitrary types
-function default_equality(a::T, b::T) where T
+function default_equality(a::T, b::T; kwargs...) where T
     vals = fieldvalues(a)
 
-    # If we don't have
+    # If we don't have any fields then we're probably dealing with scalars
     if isempty(vals)
         # Only call isapprox for numbers, otherwise we fallback to ==
-        return T <: Number ? isapprox(a, b; atol=1e-2) : a == b
+        return T <: Number ? isapprox(a, b; kwargs...) : a == b
     else
-        return all(t -> default_equality(t...), zip(vals, fieldvalues(b)))
+        return all(t -> default_equality(t...; kwargs...), zip(vals, fieldvalues(b)))
     end
 end
 
 # Handles extracting elements from arrays.
 # Needed because fieldvalues(a) are empty, but we may need to recurse depending on
 # the element type
-function default_equality(a::T, b::T) where T<:AbstractArray
-    return all(t -> default_equality(t...), zip(a, b))
+function default_equality(a::T, b::T; kwargs...) where T<:AbstractArray
+    return all(t -> default_equality(t...; kwargs...), zip(a, b))
 end
 
 # Handles extracting values for any dictionary types
-function default_equality(a::T, b::T) where T<:AbstractDict
-    return all(t -> default_equality(t...), zip(values(a), values(b)))
+function default_equality(a::T, b::T; kwargs...) where T<:AbstractDict
+    return all(t -> default_equality(t...; kwargs...), zip(values(a), values(b)))
 end
 
-function test_flatten_interface(
-    x::T;
-    check_inferred::Bool=true,
-    eltypes=(Float16, Float32, Float64),
-    equality=default_equality,
-) where {T}
-
-    # Checks default eltype still works
-    v, unflatten = flatten(x)
-    @test typeof(v) === Vector{Float64}
-    @test equality(x, unflatten(v))
-    @test unflatten(v) isa T
-
-    # Check that everything infers properly.
-    check_inferred && @inferred flatten(x)
-
-    # Test with different precisions
-    @testset "flatten($type, $T)" for type in eltypes
-        # Ensure that basic functionality is implemented.
-        v, unflatten = flatten(type, x)
-        @test typeof(v) === Vector{type}
-        @test equality(x, unflatten(v))
+# NOTE: May want to make the equality function a kwarg in the future.
+function test_flatten_interface(x::T; check_inferred::Bool=true) where T
+    @testset "flatten($T)" begin
+        # Checks default eltype still works and ensure that
+        # basic functionality is implemented.
+        v, unflatten = flatten(x)
+        @test typeof(v) === Vector{Float64}
+        @test default_equality(x, unflatten(v))
         @test unflatten(v) isa T
 
         # Check that everything infers properly.
-        check_inferred && @inferred flatten(type, x)
+        check_inferred && @inferred flatten(x)
+
+        # Test with different precisions
+        @testset "Float64" begin
+            _v, _unflatten = flatten(Float64, x)
+            @test typeof(_v) === Vector{Float64}
+            @test _v == v
+            @test default_equality(x, unflatten(_v))
+            @test _unflatten(_v) isa T
+
+            # Check that everything infers properly.
+            check_inferred && @inferred flatten(Float64, x)
+        end
+        @testset "Float32" begin
+            _v, _unflatten = flatten(Float32, x)
+            @test typeof(_v) === Vector{Float32}
+            @test default_equality(x, _unflatten(_v); atol=1e-5)
+            @test _unflatten(_v) isa T
+
+            # Check that everything infers properly.
+            check_inferred && @inferred flatten(Float32, x)
+        end
+        @testset "Float16" begin
+            _v, _unflatten = flatten(Float16, x)
+            @test typeof(_v) === Vector{Float16}
+            @test default_equality(x, _unflatten(_v); atol=1e-2)
+            @test _unflatten(_v) isa T
+
+            # Check that everything infers properly.
+            check_inferred && @inferred flatten(Float16, x)
+        end
     end
 
     return nothing
--- test/flatten.jl
+++ test/flatten.jl
@@ -10,22 +10,16 @@
     end
 
     @testset "AbstractArrays" begin
-        # We round the rand output to avoid any precision issues when testing the flatten
-        # interface (i.e., x::Float64 -> y::Float16 -> z::Float64 may result in x != z).
-        # For testing purposes we don't care about this precision loss and we just want
-        # make sure folks can use various precisions depending on the applications.
-        test_flatten_interface(round.(randn(10); digits=3))
-        test_flatten_interface(round.(randn(5, 4); digits=3))
-        test_flatten_interface([round.(randn(5); digits=3) for _ in 1:3])
+        test_flatten_interface(randn(10))
+        test_flatten_interface(randn(5, 4))
+        test_flatten_interface([randn(5) for _ in 1:3])
     end
 
-
     @testset "Tuple" begin
-
         test_flatten_interface((1.0, 2.0); check_inferred=tuple_infers)
 
         test_flatten_interface(
-            (1.0, (2.0, 3.0), round.(randn(5); digits=3)); check_inferred=tuple_infers,
+            (1.0, (2.0, 3.0), randn(5)); check_inferred=tuple_infers,
         )
     end
 
