58a44c1de28122e389a295ede6fbce2b82f50758
Jonnie Diegelman <47193959+jonniedie@users.noreply.github.com>
2021-09-03 15:14:29 -0400

    Formatting changes suggested by JuliaFormatter

--- src/ParameterHandling.jl
+++ src/ParameterHandling.jl
@@ -6,7 +6,8 @@ using ChainRulesCore
 using LinearAlgebra
 using SparseArrays
 
-export flatten, value_flatten, positive, bounded, fixed, deferred, orthogonal, positive_definite
+export flatten,
+    value_flatten, positive, bounded, fixed, deferred, orthogonal, positive_definite
 
 include("flatten.jl")
 include("parameters.jl")
