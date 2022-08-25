dd13a55f68cd666f05315de8aff22f422038ad1e
Will Tebbutt <will.tebbutt@invenialabs.co.uk>
2022-08-25 14:37:42 +0100

    Bounded Array + Zygote performance (#55)
    * Implement bounded for array
    
    * Bump patch
    
    * Fix positive test
    
    * Improve readme
    
    * Formatting
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Reduce test-case size
    
    * Loosen allocation bound
    
    * Fix test
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/parameters_array.jl
+++ src/parameters_array.jl
@@ -32,3 +32,55 @@ function positive(val::Array{<:Real}, transform=exp, ε=sqrt(eps(eltype(val))))
         unconstrained_value, transform, convert(eltype(unconstrained_value), ε)
     )
 end
+
+struct BoundedArray{T<:Real,Ta<:AbstractArray{T},V,Tε<:Real} <: AbstractParameter
+    unconstrained_value::Ta
+    lower_bound::T
+    upper_bound::T
+    transform::V
+    ε::Tε
+end
+
+value(x::BoundedArray) = x.transform(x.unconstrained_value)
+
+function flatten(::Type{T}, x::BoundedArray) where {T<:Real}
+    v, unflatten_to_Array = flatten(T, x.unconstrained_value)
+
+    function unflatten_Bounded(v_new::Vector{T})
+        return BoundedArray(
+            unflatten_to_Array(v_new), x.lower_bound, x.upper_bound, x.transform, x.ε
+        )
+    end
+
+    return v, unflatten_Bounded
+end
+
+"""
+    bounded(val::Array{<:Real}, lower_bound::Real, upper_bound::Real)
+
+Roughly equivalent to `bounded.(val, lower_bound, upper_bound)`, but implemented such that
+unflattening can be efficiently differentiated through using algorithmic differentiation
+(Zygote in particular).
+"""
+function bounded(val::Array{<:Real}, lower_bound::Real, upper_bound::Real)
+    lb = convert(eltype(val), lower_bound)
+    ub = convert(eltype(val), upper_bound)
+
+    # construct open interval
+    ε = convert(eltype(val), 1e-12)
+    lb_plus_ε = lb + ε
+    ub_minus_ε = ub - ε
+
+    if any(val .> ub_minus_ε) || any(val .< lb_plus_ε)
+        throw(
+            ArgumentError("At least one element of `val`, $val, outside of specified bounds
+                          ($lower_bound, $upper_bound).")
+        )
+    end
+
+    length_interval = ub_minus_ε - lb_plus_ε
+    unconstrained_val = logit.((val .- lb_plus_ε) ./ length_interval)
+    transform(x) = lb_plus_ε .+ length_interval .* logistic.(x)
+
+    return BoundedArray(unconstrained_val, lb, ub, transform, ε)
+end
