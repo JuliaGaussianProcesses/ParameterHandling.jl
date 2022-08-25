3bc6880bffe219ac495ba6c84011dbd4edb7ae15
Will Tebbutt <will.tebbutt@invenialabs.co.uk>
2022-08-25 10:48:45 +0100

    Include allocation counter

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,6 +14,11 @@ using ParameterHandling.TestUtils: test_flatten_interface, test_parameter_interf
 
 const tuple_infers = VERSION < v"1.5" ? false : true
 
+function count_allocs(f, args...)
+    stats = @timed f(args...)
+    return Base.gc_alloc_count(stats.gcstats)
+end
+
 @testset "ParameterHandling.jl" begin
     include("flatten.jl")
     include("parameters.jl")
